76f1d1502395c0439445f90cf16ae907e4791d79
MoutOgm <bouttier29.alex@orange.fr>
2024-11-06 21:18:41 +0100

    Removing Makefile and add lib folder mkdir

--- compil.sh
+++ compil.sh
@@ -3,6 +3,9 @@ cd ./rc_lib
 echo "-- building lib"
 cargo build --release
 echo "-- copying lib"
+if [ ! -d "../lib" ]; then
+    mkdir ../lib
+fi
 cp ./target/release/librc.a ../lib/
 echo "-- create header.hpp"
 cbindgen --config ../cbindgen.toml --crate rc --output ../cproject/includes/lib/rclib.hpp
