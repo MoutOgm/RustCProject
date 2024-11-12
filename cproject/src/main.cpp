bb615b7e1201b92638757b346cd78c4e191e0376
MoutOgm <bouttier29.alex@orange.fr>
2024-11-12 18:50:51 +0100

    Correction des chemins dans .gitignore et CMakeLists.txt, ajout de la fonction pour trouver les bibliothèques Rust, et mise à jour des commandes de copie. Suppression du fichier utils.h.

--- cproject/src/main.cpp
+++ cproject/src/main.cpp
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include "lib/utils.h"
-
+#include "lib/utils.hpp"
 
 int main() {
-    rc::utils::T t = rc::utils::T {1, 2};
+    rc::utils::T t = rc::utils::T{1, 2};
     printf("add: %d\n", t.add());
     return 0;
 }
