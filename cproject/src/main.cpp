d12edb76d29d43c02f775952684c994c1ec8f81a
MoutOgm <bouttier29.alex@orange.fr>
2024-11-12 19:19:38 +0100

    use build-cxx lib tag for a lib

--- cproject/src/main.cpp
+++ cproject/src/main.cpp
@@ -2,7 +2,5 @@
 #include "lib/utils.hpp"
 
 int main() {
-    rc::utils::T t = rc::utils::T{1, 2};
-    printf("add: %d\n", t.add());
     return 0;
 }
--- rc_lib/utils/src/lib.rs
+++ rc_lib/utils/src/lib.rs
@@ -1,22 +1,5 @@
 #[cxx::bridge(namespace = "rc::utils")]
 mod ffi {
-
-    struct T {
-        a: i32,
-        b: i32,
-    }
-
     extern "Rust" {
-        fn add(self: &T) -> i32;
-        fn n() -> T;
     }
-}
-
-impl ffi::T {
-    fn add(&self) -> i32 {
-        self.a + self.b
-    }
-}
-fn n() -> ffi::T {
-    ffi::T { a: 1, b: 2 }
 }
